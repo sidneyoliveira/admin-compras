9170475446d6591f90fa91e5c18223800f5e095f
sidneyoliveira <sidneyoliveira2015@gmail.com>
2023-09-11 19:50:55 -0300

    aa

--- script.js
+++ script.js
@@ -29,4 +29,34 @@ sidebarToggle.addEventListener("click", () => {
     }else{
         localStorage.setItem("status", "open");
     }
-})
+});
+
+
+const fs = require('fs');
+
+// Nome do arquivo de texto a ser lido
+const fileName = 'data.txt';
+
+// Lê o arquivo de texto
+fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+        console.error(`Erro ao ler o arquivo ${fileName}: ${err}`);
+        return;
+    }
+
+    console.log(`Conteúdo do arquivo ${fileName}:\n${data}`);
+});
+
+import data from ".data.sql"
+
+var table = $('#table_1').DataTable({
+       'data': data,
+       'columnDefs': [
+          {
+             'targets': 0,
+             'checkboxes': true
+          }
+       ],
+       'order': [[1, 'asc']]
+});
+ 
